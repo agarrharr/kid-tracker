6ea7354bd147d26011c51dd8eb07afb160b21ef2
Adam Garrett-Harris <agarrharr@users.noreply.github.com>
2019-04-12 21:15:09 -0600

    Add nodemon to watch files

--- build/index.js
+++ build/index.js
@@ -3,8 +3,8 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var express = require("express");
 var app = express();
 var port = process.env.PORT || 3000;
-app.get('/', function (req, res) {
-    res.send('Welcome!');
+app.get("/", function (req, res) {
+    res.send("Welcome!");
 });
 app.listen(port, function () {
     console.log("Running on port " + port);
